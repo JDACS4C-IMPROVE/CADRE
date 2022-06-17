2834e6a68c8c19dc218a2a31a3dcafddd31e6add
Oleksandr Narykov <onarykov@clswl079.cels.anl.gov>
2022-06-17 18:28:59 -0500

    Brought interface closer to the CANDLE specifications

--- cadre.py
+++ cadre.py
@@ -1,3 +1,4 @@
+import torch
 import candle
 import os
 
@@ -7,13 +8,6 @@ required = None
 additional_definitions = None
 
 
-def downoad_data():
-    candle.get_file('lincs1000.tsv', 'https://ftp.mcs.anl.gov/pub/candle/public/benchmarks/P1B1/lincs1000.tsv', datadir='./data')
-    candle.get_file(fname='P3B1_data.tar.gz', origin='https://ftp.mcs.anl.gov/pub/candle/public/benchmarks/P3B1/P3B1_data.tar.gz', unpack=True, datadir='./data')
-
-    candle.get_file('P1B1-train.csv', 'https://ftp.mcs.anl.gov/pub/candle/public/benchmarks/P1B1/P1B1.train.csv', datadir='./data')
-    candle.get_file('P1B1-dev.csv', 'https://ftp.mcs.anl.gov/pub/candle/public/benchmarks/P1B1/P1B1.dev.train.csv', datadir='./data')
-
 class CADRE(candle.Benchmark):
     required = None
 
@@ -22,7 +16,3 @@ class CADRE(candle.Benchmark):
             self.required = set(required)
         if additional_definitions is not None:
             self.additional_definitions = additional_definitions
-
-
-
-
--- cadre_candle.py
+++ cadre_candle.py
@@ -1,30 +1,131 @@
+import os
 import cadre
 import candle
 import argparse
 import subprocess
+import pickle
+import torch
 
-def initialize_parameters(config_path):
+
+from collabfilter import CF
+from utils import fill_mask, bool_ext, load_dataset, split_dataset
+
+
+def initialize_parameters(default_model='CADRE_default.txt'):
     # Build benchmark object
     cadre_common = cadre.CADRE(cadre.file_path,
-                                config_path,
-                                'torch')
-                                #prog='cadre_mlp',
-                                #desc='MNIST example')
+                               default_model,
+                               'torch',
+                               prog='cadre_mlp',
+                               desc='CADRE drug response prediction model')
 
     # Initialize parameters
     gParameters = candle.finalize_parameters(cadre_common)
 
     return gParameters
 
-def run(gParameters):
-    print(gParameters)
-    subprocess.run(['python3', 'run_cf_candle.py', '--model_label', 'cntx-attn-gdsc', '--max_iter', str(gParameters['max_iter']), '--dropout_rate', str(gParameters["dropout_rate"]), '--learning_rate', str(gParameters["learning_rate"]), '--batch_size', str(gParameters["batch_size"]) ])
+
+def load_data(args):
+    print("Loading drug dataset...")
+    train_set = pickle.load(open(args['train_set'], 'rb'))
+    test_set = pickle.load(open(args['test_set'], 'rb'))
+    ptw_ids = pickle.load(open(args['ptw_ids'], 'rb'))
+    return train_set, test_set, ptw_ids
+
+
+def run(args):
+    print(args)
+
+    args['use_cuda'] = args['use_cuda'] and torch.cuda.is_available()
+    train_set, test_set, ptw_ids = load_data(args)
+
+    # replace tgt in train_set
+    train_set['tgt'], train_set['msk'] = fill_mask(
+        train_set['tgt'], train_set['msk'])
+
+    args['exp_size'] = train_set['exp_bin'].shape[1]
+    args['mut_size'] = train_set['mut_bin'].shape[1]
+    args['cnv_size'] = train_set['cnv_bin'].shape[1]
+    args['drg_size'] = train_set['tgt'].shape[1]
+
+    if args['omic'] == 'exp':
+        args['omc_size'] = args['exp_size']
+    elif args['omic'] == 'mut':
+        args['omc_size'] = args['mut_size']
+    elif args['omic'] == 'cnv':
+        args['omc_size'] = args['cnv_size']
+
+    args['train_size'] = len(train_set['tmr'])
+    args['test_size'] = len(test_set['tmr'])
+
+    print(args)
+
+    model = CF(args)
+    model.build(ptw_ids)
+
+    if args['use_cuda']:
+        model = model.cuda()
+
+    logs = {'args': args, 'iter': [],
+            'precision': [], 'recall': [],
+            'f1score': [], 'accuracy': [], 'auc': [],
+            'precision_train': [], 'recall_train': [],
+            'f1score_train': [], 'accuracy_train': [], 'auc_train': [],
+            'loss': [], 'ptw_ids': ptw_ids}
+
+    if args['is_train']:
+        print("Training...")
+        logs = model.train(train_set, test_set,
+                           batch_size=args['batch_size'],
+                           test_batch_size=args['test_batch_size'],
+                           max_iter=args['max_iter'],
+                           test_inc_size=args['test_inc_size'],
+                           logs=logs)
+
+        labels, msks, preds, tmr, amtr = model.test(
+            test_set, test_batch_size=args['test_batch_size'])
+        labels_train, msks_train, preds_train, tmr_train, amtr_train = model.test_train(
+            train_set, test_batch_size=args['test_batch_size'])
+
+        logs["preds"] = preds
+        logs["msks"] = msks
+        logs["labels"] = labels
+        logs['tmr'] = tmr
+        logs['amtr'] = amtr
+
+        logs['preds_train'] = preds_train
+        logs['msks_train'] = msks_train
+        logs['labels_train'] = labels_train
+        logs['tmr_train'] = tmr_train
+        logs['amtr_train'] = amtr_train
+
+    else:
+        print("LR finding...")
+        logs = model.find_lr(train_set, test_set,
+                             batch_size=args['batch_size'],
+                             test_batch_size=args['test_batch_size'],
+                             max_iter=args['max_iter'],
+                             test_inc_size=args['test_inc_size'],
+                             logs=logs)
+
+    for trial in range(0, 100):
+        if os.path.exists("data/output/cf-rep/logs"+str(trial)+".pkl"):
+            continue
+    print(trial)
+    with open("data/output/cf/logs"+str(trial)+".pkl", "wb") as f:
+        pickle.dump(logs, f, protocol=2)
+
     return None
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--config_file", help="File with model parameters", type=str, default='CADRE_default.txt')
-    args = parser.parse_args()
 
-    params = initialize_parameters(args.config_file)
-    run(params)
+def main():
+    gParameters = initialize_parameters()
+    run(gParameters)
+
+
+if __name__ == "__main__":
+    main()
+    try:
+        torch.cuda.empty_cache()
+    except AttributeError:
+        pass
